0b84097d07d373a3307c842f2acb6564bcdc498a
hatouma20 <souki.hatem@gmail.com>
2020-01-22 13:36:16 +0100

    fixing warnings4

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,17 +13,10 @@ public class Greeter {
   }
 
   /**
-<<<<<<< HEAD
    * @param someone this name of person
    * @return greeting string
    */
-  public final String greet( final String someone) {
-=======
-   * @param someone this is the name of person
-   * @return greeting string
-   */  
    public final String greet(final String someone) {
->>>>>>> 9e50113627a0601e1d71cb434868891583d2184e
     return String.format("Hello, %s!", someone);
   }
 }
