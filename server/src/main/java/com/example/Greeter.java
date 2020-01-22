9e50113627a0601e1d71cb434868891583d2184e
hatouma20 <58884781+hatouma20@users.noreply.github.com>
2020-01-22 12:16:09 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,7 +16,7 @@ public class Greeter {
    * @param someone this is the name of person
    * @return greeting string
    */  
-   public String greet(String someone) {
+   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
